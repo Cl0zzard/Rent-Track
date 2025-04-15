38b0b375317332bed3049554e71bfb00f7dc1d20
Cl0zzard <janellepantoja511@gmail.com>
2025-04-15 20:43:12 +0800

    u

--- rent-tracking/topbar.php
+++
@@ -1,9 +0,0 @@
-
-<div id="header" class="shadow-sm bg-white position-sticky top-0 d-flex align-items-center px-3">
-  <div class="row w-100">
-      <div class="col-9 col-lg-5 position-relative ms-auto me-2 ps-0 pe-0">
-        <div>
-        </div>
-      </div>
-  </div>
-</div>
