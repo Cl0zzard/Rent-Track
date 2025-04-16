1f9b5431655da623b9b3708ac586a895969a7758
Shypr3AM <michelle.escarrilla2002@gmail.com>
2025-04-16 21:12:09 +0800

    Add files via upload

--- rent-tracking/database/rent_tracking.sql
+++ rent-tracking/database/rent_tracking.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2025 at 12:42 PM
+-- Generation Time: Apr 16, 2025 at 03:10 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
